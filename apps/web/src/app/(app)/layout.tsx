7318d2b598921672a5f57351f5e470fb1d697016
Krekinha <krekmg@gmail.com>
2025-02-19 04:30:37 -0300

    fe-aula-24

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,14 +1,22 @@
 import { isAuthenticated } from "@/auth/auth";
 import { redirect } from "next/navigation";
+import type React from "react";
 
 export default function AppLayout({
 	children,
+	teste,
 }: Readonly<{
 	children: React.ReactNode;
+	teste: React.ReactNode;
 }>) {
 	if (!isAuthenticated()) {
 		redirect("/auth/sign-in");
 	}
 
-	return <>{children}</>;
+	return (
+		<>
+			{children}
+			{teste}
+		</>
+	);
 }
